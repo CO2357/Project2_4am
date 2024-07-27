58e0bec3c1afafa44c426ff310be8416e3ce49f4
SSTedTan <ted_tan@s2023.ssts.edu.sg>
2024-07-27 10:47:03 +0800

    WarmUpView
    the button is commented as I didnt link view yet. soo yeah.. :D

---
+++ Project2_4am/WarmUpView.swift
@@ -0,0 +1,18 @@
+//
+//  WarmUpView.swift
+//  Project2_4am
+//
+//  Created by Ted Tan on 27/7/24.
+//
+
+import SwiftUI
+
+struct WarmUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WarmUpView()
+}
