10047311bf65428925bdd81a5365424563d95223
SSTedTan <ted_tan@s2023.ssts.edu.sg>
2024-08-10 10:35:26 +0800

    HomeView

---
+++ Project2_4am/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Project2_4am
+//
+//  Created by Ted Tan on 10/8/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
