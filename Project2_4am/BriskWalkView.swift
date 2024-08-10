92271aba37e44df55640424fffd7f241d2d672fa
Andrew <andrewtanleon791@gmail.com>
2024-08-10 11:35:43 +0800

    Added brisk walk view, linked warm up view to sports views

---
+++ Project2_4am/BriskWalkView.swift
@@ -0,0 +1,18 @@
+//
+//  BriskWalkView.swift
+//  Project2_4am
+//
+//  Created by Andrew Tan Li-Onn on 10/8/24.
+//
+
+import SwiftUI
+
+struct BriskWalkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BriskWalkView()
+}
