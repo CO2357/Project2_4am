1152aeee7b347553c8309ae82dc8f574104548b7
SSTedTan <ted_tan@s2023.ssts.edu.sg>
2024-08-10 10:58:01 +0800

    homeView

--- Project2_4am/HomeView.swift
+++ Project2_4am/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  HomeView.swift
+//  homeView.swift
 //  Project2_4am
 //
 //  Created by Ted Tan on 10/8/24.
@@ -7,12 +7,49 @@
 
 import SwiftUI
 
-struct HomeView: View {
+var circlees: Double = 0.7
+
+struct homeView: View {
+
+    let progress: Double
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView { // Wrap your view in a NavigationView
+            VStack{
+                ZStack {
+                    Circle()
+                        .stroke(
+                            Color.pink.opacity(0.5),
+                            lineWidth: 20
+                        )
+                    Circle()
+                        .trim(from: 0, to: progress)
+                        .stroke(
+                            Color.pink,
+                            style: StrokeStyle(
+                                lineWidth: 20,
+                                lineCap: .round
+                            )
+                        )
+                        .rotationEffect(.degrees(-90))
+                        .animation(.easeOut, value: progress)
+                }
+                .padding(30)
+                NavigationView{
+                    VStack{
+                        List{
+                            Text("Blacj")
+                            Text("Ni")
+                        }
+                    }
+                    .navigationTitle("Bookmarks")
+
+                }
+            }
+            .navigationTitle("Home") // Now this should work
+        }
     }
 }
 
-#Preview {
-    HomeView()
-}
+#Preview{
+    homeView(progress: circlees) }
