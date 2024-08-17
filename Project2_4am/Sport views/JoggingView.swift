2c02335a3dfe08b88ebe54c5099c452c8d96ce96
Yuk Jun Lim <limyukjun@gmail.com>
2024-08-17 09:38:26 +0800

    removed .padding()

--- Project2_4am/Sport views/JoggingView.swift
+++ Project2_4am/Sport views/JoggingView.swift
@@ -52,7 +52,6 @@ struct JoggingView: View {
                         // Handle start activity action
                     }
                 }
-                .padding()
             }
             .navigationTitle("Jogging")
         
